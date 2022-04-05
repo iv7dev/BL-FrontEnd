616b963e933fd6d9a296ef7c1531f6f82c35afa7
golmaker <iv7.software@gmail.com>
2022-04-04 22:03:30 -0300

    fix package sdk

--- src/utils/shouldShowSwapWarning.ts
+++ src/utils/shouldShowSwapWarning.ts
@@ -1,4 +1,4 @@
-import { Token } from '@pancakeswap/sdk'
+import { Token } from '@genesysnetwork/sdk'
 import SwapWarningTokens from 'config/constants/swapWarningTokens'
 
 const swapWarningTokens = Object.values(SwapWarningTokens)
